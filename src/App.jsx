aa15465e5580a407d0ca2dff921f46ff11120c0f
Priyanshu Singh <singhpriyanshu305@gmail.com>
2023-11-17 21:26:31 +0530

    vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { ClassNames } from '@emotion/react'
+import Navigation from './customer/components/navigation/Navigation'
+import MainCrousel from './customer/components/HomeCarousel/MainCarousel'
+
+function App() {
+
+  return (
+    < div className="">
+      <Navigation/>
+      <MainCrousel/>
+      <div>Home</div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainCarousel from '../../components/HomeCarousel/MainCarousel'
+
+const HomePage = () => {
+  return (
+    <div><MainCarousel/>
+    <div>
+        Other section
+    </div>
+    </div>
+  )
+}
+
+export default HomePage
