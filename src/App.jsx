07700da67d4e0710aafe3b6a6b14dcee8eb269bf
Priyanshu Singh <singhpriyanshu305@gmail.com>
2023-12-08 23:42:36 +0530

    product details

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { ClassNames } from "@emotion/react";
 import Navigation from "./customer/components/Navigation/Navigation";
 // import HomePage from "./customer/pages/HomePage/HomePage";
 import Footer from "./customer/components/Footer/Footer";
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
-import Product from "./customer/components/Product/Product";
+import ProductDetails from "./customer/components/ProductDetails/ProductDetails";
+import ProductReviewCard from "./customer/components/ProductDetails/ProductReviewCard";
+// import Product from "./customer/components/Product/Product";
 
 function App() {
   return (
     <div className="">
       <Navigation/>
       <div>
-      <Product/>
-
+      {/* <Product/> */}
+      <ProductDetails/>
+      {/* <ProductReviewCard></ProductReviewCard> */}
       {/* <HomePage /> */}
       </div>
       <Footer/>
--- src/customer/components/navigation/Navigation.jsx
+++ src/customer/components/navigation/Navigation.jsx
@@ -204,7 +204,7 @@ export default function Navigation() {
                   <span className="sr-only">Your Company</span>
                   <img
                     className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    src="/logo.svg"
                     alt=""
                   />
                   {/* </a> */}
