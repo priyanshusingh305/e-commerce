f5e42ec28b9a7c0d83d66fe973cc8c166ef87a5b
Priyanshu Singh <singhpriyanshu305@gmail.com>
2023-12-08 10:38:55 +0530

    product page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { ClassNames } from "@emotion/react";
 import Navigation from "./customer/components/Navigation/Navigation";
 // import HomePage from "./customer/pages/HomePage/HomePage";
 import Footer from "./customer/components/Footer/Footer";
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import Product from "./customer/components/Product/Product";
 
 function App() {
--- src/customer/components/Product/ProductCard.jsx
+++ src/customer/components/Product/ProductCard.jsx
@@ -1,31 +1,26 @@
 import React from "react";
-import "./ProductCard.css"
+import "./ProductCard.css";
 
-const ProductCard = () => {
+const ProductCard = ({ product }) => {
   return (
     <div className="productCard w-[15rem] m-3 transition-all cursor-pointer ">
       <div className="h-[20rem]">
-        <img className="h-full w-full object-cover object-left-top"
-          src="https://rukminim1.flixcart.com/image/612/612/kpodocw0/t-shirt/x/o/4/xl-wr-64-wrodss-original-imag3upwgq9n9fbv.jpeg?q=70"
+        <img
+          className="h-full w-full object-cover object-left-top"
+          src={product.imageUrl}
           alt=""
         />
-      </div> 
+      </div>
       <div className="textPart bg-white p-3">
-      <div>
-      <p className="font-bold opacity-60">
-      SG LEMAN
-      </p>
-        <p>
-        Men Embroidered Jacquard Straight Kurta 
-        </p>
-      </div>
-      <div className="flex items-center space-x-2">
-        <p className="font-semibold">₹199</p>
-        <p className="line-through opacity-50">₹19999</p>
-        <p className="text-green-600 font-semibold">80% off</p>
- 
-      </div>
-
+        <div>
+          <p className="font-bold opacity-60">{product.brand}</p>
+          <p>{product.title}</p>
+        </div>
+        <div className="flex items-center space-x-2">
+          <p className="font-semibold">₹{product.discountedPrice}</p>
+          <p className="line-through opacity-50">₹{product.price}</p>
+          <p className="text-green-600 font-semibold">{product.discountPersent}% off</p>
+        </div>
       </div>
     </div>
   );
