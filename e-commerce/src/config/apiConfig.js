d5fc611b31f5d10dd376b937e095e91a183d002a
Priyanshu Singh <singhpriyanshu305@gmail.com>
2024-01-03 22:22:14 +0530

    env implementation

--- e-commerce/src/config/apiConfig.js
+++ e-commerce/src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "http://localhost:5454"
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 const jwt=localStorage.getItem("jwt")
 
