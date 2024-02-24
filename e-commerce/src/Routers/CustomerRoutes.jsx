2a0f001d75714e1acf04e355698eed40ad220e2e
Priyanshu Singh <singhpriyanshu305@gmail.com>
2024-02-24 20:19:49 +0530

    route

--- e-commerce/src/Routers/CustomerRoutes.jsx
+++ e-commerce/src/Routers/CustomerRoutes.jsx
@@ -24,7 +24,7 @@ const CustomerRoutes = () => {
           <Route path='/login' element={<HomePage/>}></Route>
           <Route path='/register' element={<HomePage/>}></Route>
           <Route path='/cart' element={<Cart/>}></Route>
-          <Route path="/:levelOne/:levelTwo/:levelThree" element={<Product/> }></Route>
+          <Route path="/:levelOne/:levelTwo/:levelThree/" element={<Product/> }></Route>
           <Route path="/product/:productId" element={  <ProductDetails/>}></Route>
           <Route path="/checkout" element={  <Checkout/>}></Route>
           <Route path="/account/order" element={  <Order/>}></Route>
